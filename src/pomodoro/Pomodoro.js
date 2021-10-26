54f9e66f97b5d32c8e137f5376127c1a2da020a5
Marly PomPilus <85316146+AyitiCheri@users.noreply.github.com>
2021-10-26 00:44:47 -0400

    Add files via upload

---
+++ src/pomodoro/Pomodoro.js
@@ -0,0 +1,217 @@
+import React, { useState } from "react";
+import classNames from "../utils/class-names";
+import useInterval from "../utils/useInterval";
+import {minutesToDuration, secondstoDuration} from "../utils/duration";
+import Title from "./Title";
+
+
+function nextTick(prevState) {
+  const timeRemaining = Math.max(0, prevState.timeRemaining - 1);
+  return {
+    ...prevState,
+    timeRemaining,
+  };
+}
+
+
+function nextSession(focusDuration, breakDuration) {
+  
+  return (currentSession) => {
+    if (currentSession.label === "Focusing") {
+      return {
+        label: "On Break",
+        timeRemaining: breakDuration * 60,
+      };
+    }
+    return {
+      label: "Focusing",
+      timeRemaining: focusDuration * 60,
+    };
+  };
+}
+
+function Pomodoro() {
+  
+  const [isTimerRunning, setIsTimerRunning] = useState(false);
+  const [session, setSession] = useState(null);
+  
+  const [focusDuration, setFocusDuration] = useState(25);
+  const [breakDuration, setFocusDuration] = useState(5);
+
+  
+  useInterval(() => {
+      if (session.timeRemaining === 0) {
+        new Audio("https://bigsoundbank.com/UPLOAD/mp3/1482.mp3").play();
+        return setSession(nextSession(focusDuration, breakDuration));
+      }
+      return setSession(nextTick);
+    },
+    isTimerRunning ? 1000 : null
+  );
+
+  
+  function playPause() {
+    setIsTimerRunning((prevState) => {
+      const nextState = !prevState;
+      if (nextState) {
+        setSession((prevStateSession) => {
+          
+          if (prevStateSession === null) {
+            return {
+              label: "Focusing",
+              timeRemaining: focusDuration * 60,
+            };
+          }
+          return prevStateSession;
+        });
+      }
+      return nextState;
+    });
+  }
+
+  function handleDecreaseFocus() {
+    if (focusDuration === 5) return;
+    setFocusDuration((duration) => duration - 5);
+  }
+
+  function handleIncreaseFocus() {
+    if (focusDuration === 60) return;
+    setFocusDuration((duration) => duration + 5);
+  }
+
+  function handleDecreaseBreak() {
+    if (breakDuration === 1) return;
+    setBreakDuration((duration) => duration - 1);
+  }
+
+  function handleIncreaseBreak() {
+    if (breakDuration === 15) return;
+    setBreakDuration((duration) => duration + 1);
+  }
+
+  function handleStop() {
+    setIsTimerRunning(false);
+    setSession(null);
+  }
+
+  return (
+    <div className="pomodoro">
+      <div className="row">
+        <div className="col">
+
+          <div className="input-group input-group-lg mb-2">
+            <span className="input-group-text" data-testid="duration-focus">
+              Focus Duration: {minutesToDuration(focusDuration)}
+            </span>
+            <div className="input-group-append">
+              
+              <button
+                type="button"
+                className="btn btn-secondary"
+                data-testid="decrease-focus"
+                onClick={handleDecreaseFocus}
+                disabled={session}
+              >
+                <span className="oi oi-minus" />
+              </button>
+              
+              <button
+                type="button"
+                className="btn btn-secondary"
+                data-testid="increase-focus"
+                onClick={handleIncreaseFocus}
+                disabled={session}
+              >
+                <span className="oi oi-plus" />
+              </button>
+            </div>
+          </div>
+        </div>
+
+        <div className="col">
+          <div className="float-right">
+            <div className="input-group input-group-lg mb-2">
+              <span className="input-group-text" data-testid="duration-break">
+                
+                Break Duration: {minutesToDuration(breakDuration)}
+              </span>
+              <div className="input-group-append">
+                
+                <button
+                  type="button"
+                  className="btn btn-secondary"
+                  data-testid="decrease-break"
+                  onClick={handleDecreaseBreak}
+                  disabled={session}
+                >
+                  <span className="oi oi-minus" />
+                </button>
+                
+                <button
+                  type="button"
+                  className="btn btn-secondary"
+                  data-testid="increase-break"
+                  onClick={handleIncreaseBreak}
+                  disabled={session}
+                >
+                  <span className="oi oi-plus" />
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="row">
+        <div className="col">
+          <div
+            className="btn-group btn-group-lg mb-2"
+            role="group"
+            aria-label="Timer controls"
+          >
+            <button
+              type="button"
+              className="btn btn-primary"
+              data-testid="play-pause"
+              title="Start or pause timer"
+              onClick={playPause}
+            >
+              <span
+                className={classNames({
+                  oi: true,
+                  "oi-media-play": !isTimerRunning,
+                  "oi-media-pause": isTimerRunning,
+                })}
+              />
+            </button>
+            
+            <button
+              type="button"
+              className="btn btn-secondary"
+              data-testid="stop"
+              title="Stop the session"
+              disabled={!session}
+              onClick={handleStop}
+            >
+              <span className="oi oi-media-stop" />
+            </button>
+          </div>
+        </div>
+      </div>
+
+      <div>
+       <Title
+        session={session}
+        minutesToDuration={minutesToDuration}
+        focusDuration={focusDuration}
+        breakDuration={breakDuration}
+        secondstoDuration={secondstoDuration}
+        nextSession={nextSession}
+        />
+      </div>
+    </div>
+  );
+}
+
+export default Pomodoro;
+
---
+++ src/pomodoro/Title.js
@@ -0,0 +1,44 @@
+import React from "react";
+import Subtitle from "./Subtitle";
+import ProgressBar from "./ProgressBar";
+
+function Title({
+  session,
+  minutesToDuration,
+  focusDuration,
+  secondsToDuration,
+  breakDuration,
+}) {
+  if (session) {
+    return (
+      <>
+        <div className="row mb-2">
+          <div className="col">
+           
+            <h2 data-testid="session-title">
+              {session.label} for{" "}
+              {minutesToDuration(
+                session.label === "Focusing" ? focusDuration : breakDuration
+              )}{" "}
+              minutes
+            </h2>
+
+            <Subtitle 
+            session={session} 
+            secondsToDuration={secondsToDuration} />
+
+          </div>
+        </div>
+        <ProgressBar
+          session={session}
+          focusDuration={focusDuration}
+          breakDuration={breakDuration}
+        />
+      </>
+    );
+  } else {
+    return null;
+  }
+}
+
+export default Title;
