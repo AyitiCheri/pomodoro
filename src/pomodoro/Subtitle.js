ba7c93d63fa870b4095823602b53b97ca43c05fb
Marly PomPilus <85316146+AyitiCheri@users.noreply.github.com>
2021-10-27 21:17:01 -0400

    Update Subtitle.js

--- src/pomodoro/Subtitle.js
+++ src/pomodoro/Subtitle.js
@@ -4,7 +4,7 @@ function Subtitle({ session, secondsToDuration }) {
 
 return (
    <p className="lead" data-testid="session-sub-title">
-   {secondsToDuration(session?.timeRemaining)} remaining
+   {secondsToDuration(session.timeRemaining)} remaining
    </p>
 );
 }
